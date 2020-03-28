22fa2ca59fb9c5b88340cbc3402c2bc149f70f31
Buggitt <buggitt@me.com>
2020-03-28 02:19:30 -0700

    Update mcstatus.js

--- mcstatus.js
+++ mcstatus.js
@@ -8,8 +8,10 @@ $.getJSON(url, function(r) {
    return false;
  } 
 var pl = '';
- if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
-  $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
+ if(r.players.sample.length > 0 ){
+     pl = '<br>OP: '+r.players.sample[0].name;  
+ } 
+  $('#rest').html("MOTD: "+r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
  $('#favicon').attr('src', r.favicon);
     
 });
